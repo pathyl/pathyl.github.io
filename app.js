697f0889a7b77b2fee6cd7bcde9c0888fdf3faab
Pat <pathyl@users.noreply.github.com>
2018-04-15 13:56:46 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+require('./app_api/models/db');
+
+var routes = require('./app_server/routes/index');
+var routesApi = require('./app_api/routes/index');
+
+var app = express();
+
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+// view engine setup
+app.set('views', path.join(__dirname,'app_server', 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+
+/*use normal routes when the route begins with / */
+app.use('/', routes);
+
+/*use api routes only when the route starts with /api/*/
+//passes post data to urlencodedParser middleware first which then passes it to req.body
+app.use('/api', routesApi);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
